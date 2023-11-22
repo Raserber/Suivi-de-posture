ded8f2ad083fbc1625a975b89728138c91295e8a
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2023-11-22 16:48:15 +0100

    ajout websocket + smooth changement position

---
+++ public/js/jsFunctions.js
@@ -0,0 +1,20 @@
+function searchAndReturnEndDevice(receiveArray, deviceNumber) {
+
+    receiveArray = JSON.parse(receiveArray.data)
+
+    for (let i=0; i < receiveArray.length; i++) {
+       if (receiveArray[i].devEUI === "f1f2f3430100000" + deviceNumber) {
+
+          receiveArray[i].data.angleX *= 180/Math.PI
+          receiveArray[i].data.angleY *= 180/Math.PI
+          receiveArray[i].data.angleZ *= 180/Math.PI
+
+           return receiveArray[i].data;
+       }
+   }
+ }
+
+ function posture(CORPS, JAMBE) {
+
+    return {"version":7,"data":[[0,3.8,0],[CORPS,-90,0],[0,0,-2],[0,0,5],[6,0,JAMBE],[0],[-6,-6,-0.6],[-6,0,JAMBE],[0],[6,6,-0.6],[7,-0.6,-5],[15],[5,0,0],[-90,70,75,0,10,0,10],[0,0,10,0,10,0,10],[0,0,10,0,10,0,10],[0,0,10,0,10,0,10],[0,0,10,0,10,0,10],[-7,0.6,-5],[15],[-5,0,0],[90,-70,75,0,10,0,10],[0,0,10,0,10,0,10],[0,0,10,0,10,0,10],[0,0,10,0,10,0,10],[0,0,10,0,10,0,10]]}
+ }
