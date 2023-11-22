fda150ccd622a2132afd4bea9b23f440952ee844
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2023-11-22 17:00:51 +0100

    change position camera + rm points rouges

--- public/js/initVisualPoints.js
+++ public/js/initVisualPoints.js
@@ -1,5 +1,5 @@
-
 // initialisation des parametres Threejs
+
 geometry = new THREE.SphereGeometry(0.5);
 material = new THREE.MeshBasicMaterial({color: 'crimson'});
 material2 = new THREE.MeshBasicMaterial({color: 'black'});
@@ -36,3 +36,41 @@ head3 = new THREE.Mesh(geometry, material);
 // mains
 lfinger1 = new THREE.Mesh(geometry, material2);
 rfinger1 = new THREE.Mesh(geometry, material2);
+
+
+// ajout des points visuels à la scene Threejs (./js/initVisualPoints.js)
+// scene.add(lfeet1, lfeet2, lfeet3, lfeet4, rfeet1, rfeet2, rfeet3, rfeet4, lleg1, lleg2, rleg1, rleg2, torso1, torso2, pelvis1, pelvis2, head1, head2, head3, lfinger1, rfinger1)
+      
+
+// affichage des points rouges PIEDS
+// lfeet1.position.copy(man.l_ankle.point(6, 2, 0));
+// lfeet2.position.copy(man.l_ankle.point(-2, 2.5, 0));
+// lfeet3.position.copy(man.l_ankle.point(2, 2.5, 2));
+// lfeet4.position.copy(man.l_ankle.point(2, 2.5, -2));
+// rfeet1.position.copy(man.r_ankle.point(6, 2, 0));
+// rfeet2.position.copy(man.r_ankle.point(-2, 2.5, 0));
+// rfeet3.position.copy(man.r_ankle.point(2, 2.5, 2));
+// rfeet4.position.copy(man.r_ankle.point(2, 2.5, -2));
+
+// affichage des points rouges JAMBES
+// lleg1.position.copy(man.l_leg.point(2.75, -5, 0));
+// lleg2.position.copy(man.l_leg.point(-2.75, -5, 0));
+// rleg1.position.copy(man.r_leg.point(2.75, -5, 0));
+// rleg2.position.copy(man.r_leg.point(-2.75, -5, 0));
+
+// affichage des points rouges TORSE
+// torso1.position.copy(man.torso.point(3.2, 11.3, 0));
+// torso2.position.copy(man.torso.point(-3.5, 13, 0));
+
+// affichage des points rouges PELVIEN
+// pelvis1.position.copy(man.pelvis.point(-5, -0.5, 0))
+// pelvis2.position.copy(man.pelvis.point(-1.5, -4, 0))
+
+// affichage des points rouges TETE
+// head1.position.copy(man.head.point(0, 7, 0))
+// head2.position.copy(man.head.point(3, 1.75, 0))
+// head3.position.copy(man.head.point(-3.5, 4, 0))
+
+// affichages des points noires MAINS
+// lfinger1.position.copy(man.l_wrist.point(1, 5, 0))
+// rfinger1.position.copy(man.r_wrist.point(1, 5, 0))
