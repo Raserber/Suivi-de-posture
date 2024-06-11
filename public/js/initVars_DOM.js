146d2ae06b63f8d53c15e432ad36d77bf89026b6
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-06-11 10:43:38 +0200

    meilleurs noms variables +  onclick remplace addEv

--- public/js/initVars_DOM.js
+++ public/js/initVars_DOM.js
@@ -1,11 +1,10 @@
-var angleTorse = 0, angleJambes = 0, angleTibias = 0
-rangeTorse.value = 50; rangeJambes.value = 50; rangeGenoux.value = 50
-var numeroCapteurTorse = -1, numeroCapteurCuisses = -1, numeroCapteurTibias = -1
+var angleTorse = 0, angleCuisses = 0, angleJambes = 0
+rangeTorse.value = 50; rangeCuisses.value = 50; rangeJambes.value = 50
+var numeroCapteurTorse = -1, numeroCapteurCuisses = -1, numeroCapteurJambes = -1
 var bool_3capteurs = false, boolVisibilite_leftPanel = false
 
 
 // définition des variables correspondant aux boutons et toggle (IHM)
-toggle_leftPanel = document.getElementById("toggle_leftPanel")
 commandPanel = document.getElementById("commandPanel")
 
 toggle_orbitsControls = document.getElementById("toggle_orbitsControls")
@@ -16,23 +15,15 @@ toggle_alerteAssis = document.getElementById("toggle_alerteAssis")
 
 bouton_choixNumerosDevices = document.getElementById('choixNumerosDevices')
 boutonAff_numDevices = document.getElementById('numbersDevices')
-bouton_reload = document.getElementById('reload')
 
 rangeTorse = document.getElementById('rangeTorse')
+rangeCuisses = document.getElementById('rangeCuisses')
 rangeJambes = document.getElementById('rangeJambes')
-rangeGenoux = document.getElementById('rangeGenoux')
 
 bouton_debout = document.getElementById("debout")
 bouton_assis = document.getElementById("assis")
 bouton_allonge = document.getElementById("allonge")
 
-bouton_alerte = document.getElementById("alerte")
 compteur = document.getElementById("compteur")
 compteurAlerte = document.querySelector("#compteur div")
 compteurVariable = document.querySelector("#compteur span")
-
-// EventListeners
-toggle_leftPanel.addEventListener("click", () => {boolVisibilite_leftPanel = !boolVisibilite_leftPanel})
-bouton_alerte.addEventListener("click", setTimeBeforeAlert)
-bouton_reload.addEventListener("click", reload)
-bouton_choixNumerosDevices.addEventListener("click", choixNumerosCapteurs)
