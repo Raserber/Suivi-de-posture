bc1cf80d77e0a9fe72569000d27d8b6c35c2f543
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-09-27 00:46:00 +0200

    placeholder card ./MQTTDevices

---
+++ src/components/dialogChoixBrokerMQTT/MQTTDevices.vue
@@ -0,0 +1,32 @@
+<template>
+   <v-card>
+        <v-card-title>
+            {{ title }}
+        </v-card-title>
+        
+        <v-card-subtitle>
+            {{ subtitle }}
+        </v-card-subtitle>
+        
+        <v-card-text>
+            {{ text }}
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+
+    export default {
+
+        name: "MQTTDevice",
+
+        props: {
+            title: String,
+            subtitle: String,
+            text: String
+        }
+    }
+</script>
+
+<style>
+</style>
