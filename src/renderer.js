50b2d81a69cf435a3207b7b54a131286a3019867
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-09-26 17:27:57 +0200

    Vuetify

--- src/renderer.js
+++ src/renderer.js
@@ -29,4 +29,27 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App)
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  })
+
+app.use(vuetify)
+
+app.mount('#app');
