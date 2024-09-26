398bbdfc952c3ba0cf66b89eaff929648c02dd36
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-09-26 09:47:31 +0200

    version 7-beta

--- src/components/mannequin.vue
+++ src/components/mannequin.vue
@@ -1,20 +1,20 @@
 <template>
 </template>
-  
+
 <script>
   import { Male, createStage } from '../mannequinJS/mannequin';
-
 
   export default {
     name: "mannequin",
 
     mounted() {
-      
       createStage()
 
-      var mannequin = new Male;
+      // getStage().scene.rotateX(0.3)
+      // getStage().scene.rotateY(-1.5)
+      // getStage().scene.translateY(-0.25)
       
-      mannequin.stepOnGround()
-    }
+      var mannequin = new Male
+    },
   }
 </script>
