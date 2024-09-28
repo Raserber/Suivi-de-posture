5d3dc5e9409275cd26ee88699e67b6a1e48fed17
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-09-28 18:26:41 +0200

    devices pagination

--- src/components/dialogChoixMQTT/choixDevices/index.vue
+++ src/components/dialogChoixMQTT/choixDevices/index.vue
@@ -15,11 +15,21 @@
         </v-card-title>
 
         <v-card-text>
-            <v-row dense>
-                <v-col cols="6">
-                    <!-- <MQTTDevice :title="extractUID(topic)" :subtitle="topic" :data="data"></MQTTDevice> -->
-                </v-col>
-            </v-row>
+            <v-data-iterator :items="Object.values(store.endDevices.list)" :page="page" :items-per-page="6">
+                <template v-slot:default="{ items }">
+                        <v-row dense>
+                            <v-col
+                                cols="6"
+                                v-for="(device, i) in items"
+                            >
+                                    <MQTTDevice :device="device.raw"></MQTTDevice>
+                            </v-col>
+                        </v-row>
+                </template>
+                <template v-slot:footer="{ pageCount }">
+                    <v-pagination v-model="page" :length="pageCount"></v-pagination>
+                </template>
+        </v-data-iterator>
         </v-card-text>
 
         <v-card-actions>
@@ -43,6 +53,10 @@
     export default {
 
     name: "choixDevices",
+        
+    data: () => ({
+        page: 1
+    }),
 
     setup() {
         const store = generalStore();
