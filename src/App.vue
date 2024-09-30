900e288b265e09e8dfa46167ac0d17a98e2af119
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-09-30 22:26:33 +0200

    remove endDevices de test

--- src/App.vue
+++ src/App.vue
@@ -19,32 +19,7 @@ const store = generalStore()
 window.electronAPI.onMessageMQTT(({topic, data}) => {
 
   store.endDevices.update(topic, data)
-  store.messageMQTT = {topic, data}
 })
-function createGNAGNA(topicM) {
-
-  store.endDevices.update(`application/iotize/IoTize2039209242${topicM}/uplink`,
-    {
-      accX: 1,
-      accY: 0.0003,
-      accZ: 0.012,
-      gyrX: 0.0023,
-      gyrY: 0.0013,
-      gyrZ: 0.010,
-      dt: 100
-    }
-  )
-}
-
-createGNAGNA(1)
-createGNAGNA(2)
-createGNAGNA(3)
-createGNAGNA(4)
-createGNAGNA(5)
-createGNAGNA(6)
-createGNAGNA(7)
-createGNAGNA(8)
-createGNAGNA(9)
 
 window.electronAPI.onStatutMQTT(statut => {
 
