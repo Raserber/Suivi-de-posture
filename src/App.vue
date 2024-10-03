75a533329ace7e0967034cb785ff4e89ce1165f0
Lounes Dupuis-Dabaji <dupuisdl@iut1.u-ga.fr>
2024-10-03 12:31:58 +0200

    change name snackbar->notifs & add logoIUT1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
   <Mannequin></Mannequin>
   
   <dialogChoixMQTT></dialogChoixMQTT>
-  <snackbar></snackbar>
+  <notifications></notifications>
+
+  <v-img
+    id="logoIUT1"
+    src="./src/img/IUT1.png"
+  ></v-img>
 </template>
 
 <script setup>
@@ -11,7 +16,7 @@ import { onBeforeMount } from 'vue';
 import dialogChoixMQTT from './components/dialogChoixMQTT/index.vue';
 import Mannequin from './components/mannequin.vue';
 import Parametres from './components/parametres.vue';
-import snackbar from './components/snackbar.vue';
+import notifications from './components/notifications/index.vue';
 import { generalStore } from './store';
 
 // window.electronAPI.onDonneesMQTT((mqttStatus) => {})
@@ -40,4 +45,19 @@ onBeforeMount(()=> {
 
 <style>
 
+#logoIUT1 {
+
+position: absolute;
+bottom: 0px;
+right: 0px;
+
+margin: 1.75vw;
+
+width: 25vw;
+min-width: 200px;
+
+filter: drop-shadow(rgba(255,255,255,0.25) 0px 0px 7px);
+
+z-index: 100;
+}
 </style>
---
+++ src/components/notifications/snackbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-snackbar
+    :model-value="isVisible"
+    location="top right"
+    min-width="10"
+    min-heigth="1"
+    :timer="color"
+    transition="slide-x-reverse-transition"
+    :open-delay="500"
+>
+    <v-btn
+        variant="outlined" size="small"
+        :color="color" :icon="icon"
+        :ripple="false"
+    ></v-btn>
+    {{ text }}
+</v-snackbar>
+</template>
+
+<script>
+
+    export default {
+
+        name: "snackbar",
+
+        props: {
+            isVisible: Boolean,
+            text: String,
+            icon: String,
+            color: String
+        }
+    }
+</script>
+
+<style>
+</style>
